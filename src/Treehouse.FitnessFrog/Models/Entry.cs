5649cb64d5208a491f06742a88cf3bc17fcb0299
Jizi Zhang <zjzottawa@gmail.com>
2017-05-13 00:38:37 -0400

    Added validation for notes using annotation

--- src/Treehouse.FitnessFrog/Models/Entry.cs
+++ src/Treehouse.FitnessFrog/Models/Entry.cs
@@ -89,6 +89,7 @@ namespace Treehouse.FitnessFrog.Models
         /// <summary>
         /// The notes for the entry.
         /// </summary>
+        [MaxLength(200, ErrorMessage ="200 characters only")]
         public string Notes { get; set; }
     }
 }
